fcbadcf024455d75516d88f78070921d1e2b2926
Mr. Programmer lin <m.mohammadi.developer@gmail.com>
2021-10-16 01:23:39 +0330

    inital commit

---
+++ src/AvatarateServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Avatarate;
+
+use Illuminate\Support\ServiceProvider;
+
+class AvatarateServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // $this->mergeConfigFrom(__DIR__.'/config/avatarate_config.php', 'avatarate');
+    }
+
+    public function boot()
+    {
+
+    }
+}
